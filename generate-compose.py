00ad352e06eebbe6865782987da7df08b93a74aa
Matias Merlo <mmerlog@fi.uba.ar>
2026-03-23 02:34:19 -0300

    feat(generate_compose.py): remove logging env vars
    The env vars take precedence over the config variables, so the tests dont pass

--- generate-compose.py
+++ generate-compose.py
@@ -11,7 +11,6 @@ def generate_compose(output_file, num_clients):
         "entrypoint": "python3 /main.py",
         "environment": [
             "PYTHONUNBUFFERED=1",
-            "LOGGING_LEVEL=DEBUG",
         ],
         "networks": ["testing_net"],
         "volumes": [
@@ -26,7 +25,6 @@ def generate_compose(output_file, num_clients):
             "entrypoint": "/client",
             "environment": [
                 f"CLI_ID={i}",
-                "CLI_LOG_LEVEL=DEBUG",
             ],
             "networks": ["testing_net"],
             "depends_on": ["server"],
